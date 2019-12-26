103d89ed29fa27daa85825480a65887d1cca5ce9
Artashes <artashes.b@webihub.com>
2019-12-26 16:10:33 +0400

    Behindshops_Stripe version=1.0.0

---
+++ Behindshops/Stripe/Model/Source/Cctype.php
@@ -0,0 +1,11 @@
+<?php
+namespace Behindshops\Stripe\Model\Source;
+
+class Cctype extends \Magento\Payment\Model\Source\Cctype{
+
+    public function getAllowedTypes()
+    {
+        return array('VI', 'MC', 'AE', 'DI', 'JCB', 'OT');
+    }
+    
+}
